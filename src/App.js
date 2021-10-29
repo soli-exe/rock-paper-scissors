ca171217b154269bb53fb791f49c34e7dbefe82a
soli-exe <sja.exe@gmail.com>
2021-10-29 22:28:51 +0330

    Header componen imported to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import './assets/css/Main.css';
+import style from './assets/css/App.module.css';
+
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className={style.container}>
+        <header className={style.header}>
+          <Header />
+        </header>
       </div>
     );
   }
