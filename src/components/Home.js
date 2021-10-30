52e7c331e0ecfa0ae99441fccf436cce29ed9765
soli-exe <sja.exe@gmail.com>
2021-10-30 02:30:42 -0700

    Home component hold the all components which is need to be shown on the first page

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+import Header from './Header';
+import Logos from './Logos';
+import Footer from './Footer';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className='container'>
+            <header className='header'>
+              <Header isPlaying={false} />
+            </header>
+            <div className='logosContainer'>
+              <Logos />
+            </div>
+            <footer className='footer'>
+              <Footer />
+            </footer>
+          </div>
+        );
+    }
+}
+
+export default Home;
